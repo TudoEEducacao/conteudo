0ba827b0d878e9b37d9fbe0a2ef9e3c90430a701
Igor Felipe <appsigor@gmail.com>
2016-07-21 22:36:04 -0300

    Search Filter and modal window implemented - Now its possible to separate teachers selecting a discipline or clicking in the superior menu

--- public_html/content.js
+++ public_html/content.js
@@ -3,41 +3,56 @@ angular.module("conteudo").controller("conteudoCtrl", function($scope) {
     $scope.app = "Conteudo";
     $scope.searchFilter = "";
     $scope.Exatas = [
-        {nome: "Pedro", disciplina: "Calculo I", interesse: "Exatas", link: "#Pedro"},
-        {nome: "Maria", disciplina: "Calculo II", interesse: "Exatas", link: "#Maria"},
-        {nome: "Ana", disciplina: "Calculo Numérico", interesse: "Exatas", link: "#Ana"},
-        {nome: "João", disciplina: "Engenharia de Software", interesse: "Exatas", link: "#Joao"},
-        {nome: "Roberto", disciplina: "Banco de Dados", interesse: "Exatas", link: "#Roberto"}
+        {nome: "Pedro", disciplina: "Calculo I", interesse: "Exatas", link: "#Pedro", photoLink: ""},
+        {nome: "Maria", disciplina: "Calculo II", interesse: "Exatas", link: "#Maria", photoLink: ""},
+        {nome: "Ana", disciplina: "Calculo Numérico", interesse: "Exatas", link: "#Ana", photoLink: ""},
+        {nome: "João", disciplina: "Engenharia de Software", interesse: "Exatas", link: "#Joao", photoLink: ""},
+        {nome: "Roberto", disciplina: "Banco de Dados", interesse: "Exatas", link: "#Roberto", photoLink: ""}
     ];
     
     $scope.Bio = [
-        {nome: "Darlene", disciplina: "Anatomia Humana", interesse: "Biológicas", link: "#Darlene"},
-        {nome: "Ricardo", disciplina: "Taxonomia", interesse: "Biológicas", link: "#Ricardo"},
-        {nome: "Joana", disciplina: "Botanica Geral", interesse: "Biológicas", link: "#Joana"},
-        {nome: "Helizabete", disciplina: "Bioquímica", interesse: "Biológicas", link: "#Helizabete"},
-        {nome: "Roberto", disciplina: "Embriologia", interesse: "Biológicas", link: "#Roberto"}
+        {nome: "Darlene", disciplina: "Anatomia Humana", interesse: "Biológicas", link: "#Darlene", photoLink: ""},
+        {nome: "Ricardo", disciplina: "Taxonomia", interesse: "Biológicas", link: "#Ricardo", photoLink: ""},
+        {nome: "Joana", disciplina: "Botanica Geral", interesse: "Biológicas", link: "#Joana", photoLink: ""},
+        {nome: "Helizabete", disciplina: "Bioquímica", interesse: "Biológicas", link: "#Helizabete", photoLink: ""},
+        {nome: "Roberto", disciplina: "Embriologia", interesse: "Biológicas", link: "#Roberto", photoLink: ""}
     ];
     
     $scope.Humanas = [
-        {nome: "Marcos", disciplina: "Teorias e Sistemas em Psicologia I", interesse: "Humanas", link: "#Marcos"},
-        {nome: "Gislene", disciplina: "Teorias e Sistemas em Psicologia II", interesse: "Humanas", link: "#Gislene"},
-        {nome: "Talita", disciplina: "Teoria da História", interesse: "Humanas", link: "#Talita"},
-        {nome: "Flavio", disciplina: "Metodologia da Pesquisa Histórica ", interesse: "Humanas", link: "#Flavio"},
-        {nome: "Diego", disciplina: "Sociologia Moderna", interesse: "Humanas", link: "#Diego"}
+        {nome: "Marcos", disciplina: "Teorias e Sistemas em Psicologia I", interesse: "Humanas", link: "#Marcos", photoLink: ""},
+        {nome: "Gislene", disciplina: "Teorias e Sistemas em Psicologia II", interesse: "Humanas", link: "#Gislene", photoLink: ""},
+        {nome: "Talita", disciplina: "Teoria da História", interesse: "Humanas", link: "#Talita", photoLink: ""},
+        {nome: "Flavio", disciplina: "Metodologia da Pesquisa Histórica ", interesse: "Humanas", link: "#Flavio", photoLink: ""},
+        {nome: "Diego", disciplina: "Sociologia Moderna", interesse: "Humanas", link: "#Diego", photoLink: ""}
     ];
     
     $scope.Featured = [
-        {nome: "Pedro", disciplina: "Calculo I", interesse: "Exatas", link: "#Pedro"},
-        {nome: "Ricardo", disciplina: "Taxonomia", interesse: "Biológicas", link: "#Ricardo"},
-        {nome: "Ana", disciplina: "Calculo Numérico", interesse: "Exatas", link: "#Ana"},
-        {nome: "Gislene", disciplina: "Teorias e Sistemas em Psicologia II", interesse: "Humanas", link: "#Gislene"},
-        {nome: "Roberto", disciplina: "Banco de Dados", interesse: "Exatas", link: "#Roberto"}
+        {nome: "Pedro", disciplina: "Calculo I", interesse: "Exatas", link: "#Pedro", photoLink: ""},
+        {nome: "Ricardo", disciplina: "Taxonomia", interesse: "Biológicas", link: "#Ricardo", photoLink: ""},
+        {nome: "Ana", disciplina: "Calculo Numérico", interesse: "Exatas", link: "#Ana", photoLink: ""},
+        {nome: "Gislene", disciplina: "Teorias e Sistemas em Psicologia II", interesse: "Humanas", link: "#Gislene", photoLink: ""},
+        {nome: "Roberto", disciplina: "Banco de Dados", interesse: "Exatas", link: "#Roberto", photoLink: ""}
     ];
+    
+    $scope.Avaiable = $scope.Exatas.concat($scope.Bio, $scope.Humanas);
     
     $scope.FilteredData = $scope.Featured;
     
-    $scope.addFilter = function (value) {
+    $scope.addDisciplinaFilter = function (value) {
         $scope.searchFilter = value;
+        
+        $scope.FilteredData = $scope.Avaiable.filter(function (element){
+           return element.disciplina === value; 
+        });
     };
+    
+    $scope.addInteresseFilter = function (value) {
+        $scope.searchFilter = value;
+
+        $scope.FilteredData = $scope.Avaiable.filter(function (element){
+           return element.interesse === value; 
+        });
+    };
+    
 });
 
